140c87b47bf0e47a0ba324b8cebca1e4f1d82475
Karolina Pastor <borek.karola@gmail.com>
2021-02-27 17:31:09 +0100

    Edit function Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,12 @@
 import styles from './Hero.scss';
 import React from 'react';
 
-const Hero = function(){
-  const content = (
+const Hero = props => (
     <header className={styles.component}>
-  		<h2 className={styles.title}>Things to do</h2>
+  		<h2 className={styles.title}>{props.titleText}</h2>
   		<img className={styles.image} src="https://i.postimg.cc/ZRCPy6Tm/space.png" alt="cosmos"/>
       </header>
   );
 
-  return content;
-};
 
 export default Hero;
