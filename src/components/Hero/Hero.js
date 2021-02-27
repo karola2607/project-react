21bbfe815b742d5e8fc153d15a0cf97630a72d88
Karolina Pastor <borek.karola@gmail.com>
2021-02-27 16:52:04 +0100

    Change class Hero to function

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,15 @@
 import styles from './Hero.scss';
 import React from 'react';
 
-class Hero extends React.Component {
-  render() {
-    return (
-      <header className={styles.component}>
+const Hero = function(){
+  const content = (
+    <header className={styles.component}>
   		<h2 className={styles.title}>Things to do</h2>
   		<img className={styles.image} src="https://i.postimg.cc/ZRCPy6Tm/space.png" alt="cosmos"/>
       </header>
-    )
-  }
-}
+  );
+
+  return content;
+};
 
 export default Hero;
