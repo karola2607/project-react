19d98161be2d2c30fe11413c2b954fe1a24e6d7d
Karolina Pastor <borek.karola@gmail.com>
2021-03-07 18:24:30 +0100

    Repair adding cards with Redux

--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -1,16 +1,16 @@
 import {connect} from 'react-redux';
 import Column from './Column';
 import {getCardsForList} from '../../redux/cardsRedux.js';
-import createActionAddCard from '../../redux/cardsRedux.js';
+import {createActionAddCard} from '../../redux/cardsRedux.js';
 
 const mapStateToProps = (state, props) => ({
   cards: getCardsForList(state, props.id),
 });
 
 const mapDispatchToProps = (dispatch, props) => ({
-  addCard: cards => dispatch(createActionAddCard({
+  addCard: title => dispatch(createActionAddCard({
     columnId: props.id,
-    cards,
+    title,
   })),
 });
 
